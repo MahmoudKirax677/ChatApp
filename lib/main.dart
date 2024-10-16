c43dc3f2260b9dd2794d3434cd745482ec14e075
Mahmoud2056 <engmahmoud2056@gmail.com>
2024-10-17 01:58:32 +0300

    Fix issues

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ import 'package:get/get.dart';
 import 'package:webview_flutter/webview_flutter.dart'; // Import WebView
 import 'package:zefaf/view/PackagePage.dart';
 import 'package:zefaf/view/webview.dart';
-import 'binding/PackageBinding.dart';
+import 'package:sentry_flutter/sentry_flutter.dart';
 
 const Color darkBlue = Color.fromARGB(255, 18, 32, 47);
 
@@ -27,7 +27,19 @@ void main() async {
     //   WebView.platform = SurfaceAndroidWebView();
     // }
 
-    runApp(const MyApp());
+    await SentryFlutter.init(
+      (options) {
+        options.dsn =
+            'https://14678d368c817431689437c7ec461cba@o4507961846136832.ingest.us.sentry.io/4508134887981056';
+        // Set tracesSampleRate to 1.0 to capture 100% of transactions for tracing.
+        // We recommend adjusting this value in production.
+        options.tracesSampleRate = 1.0;
+        // The sampling rate for profiling is relative to tracesSampleRate
+        // Setting to 1.0 will profile 100% of sampled transactions:
+        options.profilesSampleRate = 1.0;
+      },
+      appRunner: () => runApp(const MyApp()),
+    );
   } catch (e) {
     print('Error Main: $e');
     debugPrint('Error Main: $e');
