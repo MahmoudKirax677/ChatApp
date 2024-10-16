baf79b6b7eeab7d67aab708f5a7899758fd3a1a7
Mahmoud2056 <engmahmoud2056@gmail.com>
2024-10-17 00:49:22 +0300

    Fix issues

--- lib/main.dart
+++ lib/main.dart
@@ -16,17 +16,22 @@ import 'binding/PackageBinding.dart';
 
 const Color darkBlue = Color.fromARGB(255, 18, 32, 47);
 
-void main() async{
-  WidgetsFlutterBinding.ensureInitialized();
-  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
-  SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
-    statusBarColor: Colors.transparent,
-    statusBarBrightness: Brightness.dark,
-    statusBarIconBrightness: Brightness.dark,
-    systemNavigationBarColor: Color(0xffc52278),
-    systemNavigationBarIconBrightness: Brightness.light,
-  ));
-  runApp(const MyApp());
+void main() async {
+  try {
+    WidgetsFlutterBinding.ensureInitialized();
+    SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+    SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent,
+      statusBarBrightness: Brightness.dark,
+      statusBarIconBrightness: Brightness.dark,
+      systemNavigationBarColor: Color(0xffc52278),
+      systemNavigationBarIconBrightness: Brightness.light,
+    ));
+    runApp(const MyApp());
+  } catch (e) {
+    print('Error Main: $e');
+    debugPrint('Error Main: $e');
+  }
 }
 
 class MyApp extends StatefulWidget {
@@ -34,9 +39,8 @@ class MyApp extends StatefulWidget {
   @override
   State<MyApp> createState() => _MyAppState();
 }
+
 class _MyAppState extends State<MyApp> {
-
-
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
@@ -47,15 +51,11 @@ class _MyAppState extends State<MyApp> {
       theme: ThemeData(
           useMaterial3: true,
           visualDensity: VisualDensity.comfortable,
-          fontFamily: "Tajawal"
-      ),
-    );;
+          fontFamily: "Tajawal"),
+    );
+    ;
   }
 }
-
-
-
-
 
 class RestartWidget extends StatefulWidget {
   RestartWidget({required this.child});
