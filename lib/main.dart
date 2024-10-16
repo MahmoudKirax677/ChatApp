3f182182a0bc086a65f50126ca13efde250b5144
Mahmoud2056 <engmahmoud2056@gmail.com>
2024-10-17 01:15:13 +0300

    Fix issues

--- lib/main.dart
+++ lib/main.dart
@@ -3,16 +3,10 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:get/get.dart';
-import 'package:get/get_navigation/src/root/get_material_app.dart';
+import 'package:webview_flutter/webview_flutter.dart'; // Import WebView
 import 'package:zefaf/view/PackagePage.dart';
-
+import 'package:zefaf/view/webview.dart';
 import 'binding/PackageBinding.dart';
-
-//flutter build apk --split-per-abi
-//flutter build appbundle
-//flutter pub run flutter_launcher_icons:main
-//flutter pub pub run flutter_native_splash:create
-//flutter build ipa
 
 const Color darkBlue = Color.fromARGB(255, 18, 32, 47);
 
@@ -27,6 +21,12 @@ void main() async {
       systemNavigationBarColor: Color(0xffc52278),
       systemNavigationBarIconBrightness: Brightness.light,
     ));
+
+    // Handle platform-specific initialization for WebView
+    // if (Platform.isAndroid) {
+    //   WebView.platform = SurfaceAndroidWebView();
+    // }
+
     runApp(const MyApp());
   } catch (e) {
     print('Error Main: $e');
@@ -45,15 +45,16 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return GetMaterialApp(
       title: 'Zefaf',
-      initialBinding: PackageBinding(),
-      home: const PackagePage(),
+      home: const WebViewScreen(
+        link: 'https://lialinaapp.com/',
+      ), // Use WebViewScreen directly
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-          useMaterial3: true,
-          visualDensity: VisualDensity.comfortable,
-          fontFamily: "Tajawal"),
+        useMaterial3: true,
+        visualDensity: VisualDensity.comfortable,
+        fontFamily: "Tajawal",
+      ),
     );
-    ;
   }
 }
 
