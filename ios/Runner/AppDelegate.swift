23a762676c6dab2c8759cd6e81a0bb33d003df45
Mahmoud2056 <engmahmoud2056@gmail.com>
2024-10-17 03:34:12 +0300

    Fix issues

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,13 +1,17 @@
 import UIKit
 import Flutter
+import StoreKit
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
-  override func application(
-    _ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-  ) -> Bool {
-    GeneratedPluginRegistrant.register(with: self)
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-  }
+    override func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+        // Configure StoreKit
+        if #available(iOS 10.0, *) {
+            SKPaymentQueue.default().add(self)
+        }
+        return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+    }
 }
