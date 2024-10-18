86930ea0fbd0c8d102207e97e697cd5c8d180150
Mahmoud2056 <engmahmoud2056@gmail.com>
2024-10-18 21:36:56 +0300

    Fix issues

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,35 +1,12 @@
 import UIKit
 import Flutter
-import StoreKit
 
 @UIApplicationMain
-@objc class AppDelegate: FlutterAppDelegate, SKPaymentTransactionObserver {
+@objc class AppDelegate: FlutterAppDelegate {
     override func application(
         _ application: UIApplication,
         didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
     ) -> Bool {
-        // Configure StoreKit
-        if #available(iOS 10.0, *) {
-            SKPaymentQueue.default().add(self)
-        }
         return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-    }
-
-    // Required method for SKPaymentTransactionObserver
-    func paymentQueue(_ queue: SKPaymentQueue, updatedTransactions transactions: [SKPaymentTransaction]) {
-        for transaction in transactions {
-            switch transaction.transactionState {
-            case .purchased:
-                // Handle successful transaction
-                print("Transaction successful!")
-                SKPaymentQueue.default().finishTransaction(transaction)
-            case .failed:
-                // Handle failed transaction
-                print("Transaction failed!")
-                SKPaymentQueue.default().finishTransaction(transaction)
-            default:
-                break
-            }
-        }
     }
 }
