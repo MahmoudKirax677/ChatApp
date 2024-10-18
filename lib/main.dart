678dd55e0ef26995fcc9ba3c1e86f50577f3725f
Mahmoud2056 <engmahmoud2056@gmail.com>
2024-10-18 21:46:22 +0300

    Fix issues

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,7 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:get/get.dart';
-import 'package:webview_flutter/webview_flutter.dart'; // Import WebView
-import 'package:zefaf/view/PackagePage.dart';
-import 'package:zefaf/view/webview.dart';
- 
+
 const Color darkBlue = Color.fromARGB(255, 18, 32, 47);
 
 void main() async {
@@ -20,11 +17,6 @@ void main() async {
       systemNavigationBarColor: Color(0xffc52278),
       systemNavigationBarIconBrightness: Brightness.light,
     ));
-
-    // Handle platform-specific initialization for WebView
-    // if (Platform.isAndroid) {
-    //   WebView.platform = SurfaceAndroidWebView();
-    // }
 
     runApp(const MyApp());
   } catch (e) {
@@ -44,14 +36,32 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return GetMaterialApp(
       title: 'Zefaf',
-      home: const WebViewScreen(
-        link: 'https://lialinaapp.com/',
-      ), // Use WebViewScreen directly
+      home: const HomePage(), // استبدل الصفحة الرئيسية هنا
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         useMaterial3: true,
         visualDensity: VisualDensity.comfortable,
         fontFamily: "Tajawal",
+      ),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text(
+          'Welcome to Zefaf', // النص الترحيبي هنا
+          style: TextStyle(
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+            color: darkBlue,
+          ),
+        ),
       ),
     );
   }
